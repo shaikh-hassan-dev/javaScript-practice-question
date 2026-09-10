36023792c94e08e33c9452a1b04bdb0608c55290
Hassan Shaikh <hassanshaikh1809@gmail.com>
2026-09-10 13:24:45 +0530

    Add greet function to log 'Hello, World!'

---
+++ 4.Functions_Foundation/practice1.js
@@ -0,0 +1,9 @@
+// Create a function named `greet` that logs 'Hello, World!' to the console. Then call the function to execute it.
+
+// Declare your function
+function greet(){
+  console.log("Hello, World!");
+}
+
+// Call the function
+greet();
